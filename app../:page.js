098d1654a7858b79cf85983fd304c3ca456b5c98
messagerangelhelper-code <messagerangelhelper@gmail.com>
2026-09-13 16:32:49 -0500

    Create :page.js

---
+++ app../:page.js
@@ -0,0 +1,26 @@
+'use client';
+
+import { useState } from 'react';
+import CityMap from './CityMap';
+import LiveTrackingMap from './LiveTrackingMap';
+import HowToBookModal from './HowToBookModal';
+import ChatPanel from './ChatPanel';
+
+export default function HomePage() {
+  const [rideBooked, setRideBooked] = useState(false);
+  const [showChat, setShowChat] = useState(false);
+
+  return (
+    <main className="relative w-full h-screen bg-[#111318]">
+      <CityMap />
+
+      {rideBooked && <LiveTrackingMap />}
+
+      {!rideBooked && (
+        <HowToBookModal onBook={() => setRideBooked(true)} />
+      )}
+
+      {rideBooked && showChat && <ChatPanel />}
+    </main>
+  );
+}
